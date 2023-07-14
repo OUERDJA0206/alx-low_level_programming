d827337e5bbd56000e17fac52fc33cbeb63088b9
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-07-14 08:16:17 -0700

    print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char c, a;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (a = 'A'; a <= 'Z' ; a++)
+	{
+		putchar(a);
+	}
+		putchar('\n');
+	return (0);
+}
