7babb7ede107d342b7b799d6e9eb885151af5cf3
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-07-14 10:07:43 -0700

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,9 +8,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48 ; i <= 57 ; i++)
 	{
 		putchar(i);
+		if (i == 57)
+		{
+			continue;
+		}
 		putchar(',');
 		putchar(' ');
 	}
