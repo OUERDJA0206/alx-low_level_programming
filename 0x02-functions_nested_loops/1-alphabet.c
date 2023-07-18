6f183a3d79cd45259e81cf8f8a4401fa57cd0e8f
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-07-18 05:18:32 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,12 @@
   *
   * Return: Always 0 (Success)
   */
-int main(void)
+void print_alphabet(void)
 {
 	for (int i = 97; i <= 122; i++)
 	{
 		_putchar(i);
-		_putchar('\n');
 	}
+		_putchar('\n');
 	return (0);
 }
