0e097575e2338836e17761471a3655d713a9b37a
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-07-24 05:18:21 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _puts - function with one argument
+* @str: one char type argument
+* Description: print a string follow by a new line to stdout
+* Return: none
+*/
+void _puts(char *str)
+{
+	for ( ; *str != '\0'; *str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
