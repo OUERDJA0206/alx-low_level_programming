0484f0e66ed539652aa3d4943e3c80497d4df078
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-07-27 09:53:19 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* rot13 - function that encodes a string using rot13.
+* @a: string to encode
+* Return: a pointer to sear
+*/
+char *rot13(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		while ((a[i] >= 'a' && a[i] <= 'm') || (a[i] >= 'A' && a[i] <= 'M'))
+		{
+			a[i] += 13;
+			i++;
+		}
+		if ((a[i] >= 'n' && a[i] <= 'z') || (a[i] >= 'N' && a[i] <= 'Z'))
+			ar[i] -= 13;
+	}
+	return (a);
+}
