42f12e7deb33c058b058524d70f1673d70bf97f1
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-08-02 08:55:27 -0700

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,11 @@
 */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
+	if (*s == '\0')
 	{
 		_putchar('\0');
+		return;
 	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
