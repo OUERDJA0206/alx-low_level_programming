9b81c555919285cdc3dffdd426fb00d3c55294ae
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-08-02 18:11:13 -0700

    fix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,12 +8,13 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int wildcmp(char *s1, char *s2) {
-    if (*s1 == '\0' && *s2 == '\0')
-        return (1);
-    if (*s2 == '*')
-        return ((wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2))));
-    if (*s1 == *s2)
-        return wildcmp(s1 + 1, s2 + 1);
-    return (0);
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s2 == '*')
+		return ((wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2))));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
