cb72c725a08d251112e594a43970f3609224ff71
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-08-02 18:00:01 -0700

    fix

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _strlen - obtains length of a
+ * @s: string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen(s + 1));
+}
+/**
+ * check_palindrome - compares string vs string reverse
+ * @s: string
+ * @start: start length
+ * @end: end length
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (check_palindrome(s, start + 1, end + 1));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen(s);
+
+	return (check_palindrome(s, 0, len - 1));
+}
