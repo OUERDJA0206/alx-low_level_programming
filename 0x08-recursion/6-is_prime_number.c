a22e9802641890b82a7929e52e7acadf05cb77ea
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-08-02 17:22:34 -0700

    is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * check_prime - Makes possible to evaluate from 1 to n
+ * @n: same number as n
+ * @i: number that iterates from 1 to n
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+		return (-1);
+	if (i * i > n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (check_prime(n, i + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_prime_number(int n)
+{
+	check_prime(n, 2);
+}
