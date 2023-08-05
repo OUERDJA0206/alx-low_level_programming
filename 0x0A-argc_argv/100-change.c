e992f3683256e568aede662799986c72a8b24c6f
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-08-04 18:01:08 -0700

    fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,17 +11,17 @@ int main(int argc, char *argv[])
 	int coins = 0;
 	int i;
 	int values[] = {20, 10, 5, 2, 1};
-	int cents = atoi(argv[1]);
+	int cents = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
-		return (0);
 	}
 	for (i = 0; i < 5; i++)
 	{
