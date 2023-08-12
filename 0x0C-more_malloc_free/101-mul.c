49cf6775b708a349de4167d7479b5201da33cf57
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-08-11 17:03:55 -0700

    fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -19,6 +19,30 @@ int is_number(char *s)
 	return (1);
 }
 /**
+* print_error - Prints an error message and exits.
+*/
+void print_error(void)
+{
+	char *error = "Error\n";
+
+	while (*error)
+	{
+		_putchar(*error);
+		error++;
+	}
+	exit(98);
+}
+/**
+* print_num - Prints a number using putchar.
+* @n: The number to print.
+*/
+void print_num(unsigned long n)
+{
+	if (n / 10)
+		print_num(n / 10);
+	_putchar((n % 10) + '0');
+}
+/**
 * main - multiplies two positive numbers passed as arguments
 * @argc: count of arguments
 * @argv: array of arguments
@@ -29,18 +53,13 @@ int main(int argc, char *argv[])
 	unsigned long num1, num2, result;
 
 	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		print_error();
 	if (!is_number(argv[1]) || !is_number(argv[2]))
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		print_error();
 	num1 = atol(argv[1]);
 	num2 = atol(argv[2]);
 	result = num1 * num2;
-	printf("%lu\n", result);
+	print_num(result);
+	_putchar('\0');
 	return (0);
 }
