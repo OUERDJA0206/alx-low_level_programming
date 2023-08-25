004a453a735d3ac1483bfed52da94ec7dd0cf10f
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-08-24 18:44:52 -0700

    fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@
  */
 static void __attribute__((constructor)) pre_main(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
