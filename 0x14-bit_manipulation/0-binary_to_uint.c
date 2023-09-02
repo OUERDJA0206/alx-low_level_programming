027d49989a7b1fa097137d0f9751c3bbb9ccb6ac
YOUSSEF OUERDJA <maroc0206@gmail.com>
2023-09-01 17:38:19 -0700

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,29 +6,20 @@
  *
  * Return: Converted number
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	signed int index = 0, num = 0;
+	unsigned int decimal;
+	unsigned int i;
 
-
-	if (!b)
-		return (0);
-
-	while (b[index] != '\0')
+	for (decimal = 0, i = 0; b[i] != '\0'; i++)
 	{
-		if (b[index] != '0' && b[index] != '1')
-		{
+		if (b[i] == '1')
+			decimal = (decimal << 1) | 1;
+		else if (b[i] == '0')
+			decimal <<= 1;
+		else if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
-
-		num <<= 1;
-
-		if (b[index] & 1)
-		{
-			num += 1;
-		}
-		index += 1;
 	}
-	return (num);
+
+	return (decimal);
 }
